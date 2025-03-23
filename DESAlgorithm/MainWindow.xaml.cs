2b9c082c1517550ac8050312fcc988468cee2edf
Rafal251566 <251566@edu.p.lodz.pl>
2025-03-23 09:56:23 +0100

    Dodano operacje na kluczu i tworzenie podkluczy

--- DESAlgorithm/MainWindow.xaml.cs
+++ DESAlgorithm/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Printing.IndexedProperties;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Media3D;
 
 namespace DESAlgorithm
 {
@@ -14,7 +15,8 @@ namespace DESAlgorithm
         private static byte[] permutacjaOdwrotna = {40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47, 15, 55, 23, 63, 31, 38, 6, 46, 14, 54, 22, 62, 30, 37, 5, 45, 13, 53, 21, 61, 29,36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51, 19, 59, 27, 34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9, 49, 17, 57, 25 };
         private static byte[] pertmutacjaKompresujaca = { 14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2, 41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32 };
         private static byte[] permutacjaKlucza = { 57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44, 36, 63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22, 14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 28, 20, 12, 4 };
-
+        private static byte[] keyShifts = { 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1 };
+        private static byte[][] subKeys = new byte[16][];
         public MainWindow()
         {
             InitializeComponent();
@@ -71,6 +73,9 @@ namespace DESAlgorithm
                 rightKeyPart[i] = permutatedKey[i+permutatedKey.Length/2];
             }
 
+
+            subKeys = GetSubKeys(leftKeyPart,rightKeyPart);
+
             byte[] processedData = new byte[data.Length];
             for (int i = 0; i < data.Length; i += 8)
             {
@@ -82,6 +87,7 @@ namespace DESAlgorithm
             return processedData;
         }
 
+        //Metoda dokonuje permutacji na tablicy bitów zgodznie z zadanym rodzajem permutacji
         private static byte[] Permutate(byte[] tab, byte[] permutationTab)
         {
             byte[] temp = new byte[permutationTab.Length];
@@ -94,22 +100,37 @@ namespace DESAlgorithm
             return temp;
         }
 
-        //Metoda tworząca nową obróconą tablice w lewą stronęo zadaną ilość miejsc
-        private static byte[] RotateLeft(byte[] tab, int howMany) {
+        //Metoda tworząca nową obróconą tablice w lewą stronę o zadaną ilość miejsc
+        private static byte[] RotateLeft(byte[] tab, byte howMany) {
             byte[] temp = new byte[tab.Length];
-
             for (int i = 0; i < tab.Length; i++)
             {
-                if (i - howMany < 0)
+                if (i - keyShifts[howMany] < 0)
                 {
-                    temp[tab.Length + (i - howMany)] = tab[i];
+                    temp[tab.Length + (i - keyShifts[howMany])] = tab[i];
                 }
                 else
                 {
-                    temp[i - howMany] = tab[i];
+                    temp[i - keyShifts[howMany]] = tab[i];
                 }
             }
             return temp;
+        }
+
+        private static byte[][] GetSubKeys(byte[] leftSide, byte[] rightSide) {
+           
+            byte[][] keys = new byte[16][];
+            
+            for(int i = 0; i < 16; i++)
+            {
+                leftSide = RotateLeft(leftSide, keyShifts[i]);
+                rightSide = RotateLeft(rightSide, keyShifts[i]);
+                byte[] temp = new byte[leftSide.Length+rightSide.Length];
+                Array.Copy(leftSide, 0, temp, 0, leftSide.Length);
+                Array.Copy(rightSide, 0, temp, rightSide.Length, rightSide.Length);
+                keys[i] = Permutate(temp, pertmutacjaKompresujaca);
+            }
+            return keys;
         }
 
    
