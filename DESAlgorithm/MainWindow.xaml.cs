cbe6db83fe882cd823984d9858d4a4b0270fd4ff
Rafal251566 <251566@edu.p.lodz.pl>
2025-04-05 19:11:31 +0200

    Dodano dowolność wyboru klucza

--- DESAlgorithm/MainWindow.xaml.cs
+++ DESAlgorithm/MainWindow.xaml.cs
@@ -5,16 +5,18 @@ using System.IO;
 using System.Printing.IndexedProperties;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading.Channels;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Media.Media3D;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace DESAlgorithm
 {
     public partial class MainWindow : Window
     {
-        private static byte[] key = {0,1,1,1,0,0,1,0, 0,1,1,0,0,1,0,1, 0,1,1,1,0,1,1,1, 0,1,1,0,1,1,1,1, 0,1,1,0,1,1,0,0, 0,1,1,1,0,1,1,1, 0,1,1,0,0,1,0,1, 0,1,1,1,0,0,1,0, 0,0,0,0,1,0,1,0};
+        private static string keyString = "rewolwer";
         private static byte[] initialPermutation = {58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8, 57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3, 61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7};
         private static byte[] finalPermutation = {40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47, 15, 55, 23, 63, 31, 38, 6, 46, 14, 54, 22, 62, 30, 37, 5, 45, 13, 53, 21, 61, 29,36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51, 19, 59, 27, 34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9, 49, 17, 57, 25 };
         private static byte[] compressionPermutation = { 14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2, 41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32 };
@@ -33,6 +35,9 @@ namespace DESAlgorithm
             new byte[]{13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7,  1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2,  7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8,  2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11}
         };
         private static byte[][] subKeys = new byte[16][];
+        private static byte[] key = ConvertToBinary(Encoding.ASCII.GetBytes(keyString),8);
+
+
         public MainWindow()
         {
             InitializeComponent();
@@ -122,6 +127,7 @@ namespace DESAlgorithm
         private static byte[] DESAlgorithm(byte[] data, bool toEncryption) 
         {
             data = fillMissingBits(data);
+            Trace.WriteLine(String.Join(" ",key));
             byte[] permutatedKey = Permutate(key, keyPermutation);
             byte[] leftKeyPart = new byte[permutatedKey.Length/2];
             byte[] rightKeyPart = new byte[permutatedKey.Length/2];
